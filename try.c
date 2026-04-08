2a3421ba7ecc83a56198ad017858635d4a0fee32
rainxue47 <rainxue47@gmial.com>
2026-04-08 20:40:11 +0800

    增加了用户指引

--- try.c
+++ try.c
@@ -5,7 +5,7 @@ int main()
     for (int i = 1; i <= 10; i++) // 使用for循环打印1到10的数字
       {
         printf("%d ", i); // 打印数字后跟一个空格
-    printf(" "); // 换行
+    printf("put out: "); // 换行
       }
     return 0;
 }
